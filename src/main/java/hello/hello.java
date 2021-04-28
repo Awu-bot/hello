256dcde6b06a405a611ddaa735cf1a340b1f1688
baoningsong <794624434@qq.com>
2021-04-28 23:54:53 +0800

    第四次修改

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -4,5 +4,6 @@ public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello world!");
 	System.out.println("Hi!Jack");
+	System.out.println("Oh!it is wonderful!");
 }
 }
