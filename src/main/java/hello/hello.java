094c998364aa99bbf0d7fe00f71c540951831c2b
baoningsong <794624434@qq.com>
2021-04-29 21:53:57 +0800

    修改5

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -3,13 +3,22 @@ package hello;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello world!");
+<<<<<<< bns
 dfsd
+=======
+>>>>>>> c08370d 更新2
 	System.out.println("Hi!Jack");
+<<<<<<< bns
 
 hh
+=======
+>>>>>>> c08370d 更新2
 	System.out.println("Oh!it is wonderful!");
+<<<<<<< bns
 
 
 lk
+=======
+>>>>>>> c08370d 更新2
 }
 }
