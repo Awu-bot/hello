c08370d519bb52fbee1892adceebfad75946e32d
baoningsong <794624434@qq.com>
2021-04-29 21:28:31 +0800

    更新2

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -3,7 +3,10 @@ package hello;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello world!");
+	hhh
 	System.out.println("Hi!Jack");
+	hh
 	System.out.println("Oh!it is wonderful!");
+	jj
 }
 }
